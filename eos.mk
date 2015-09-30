a102a0fdfff73b59d14eefd4bd5b594f813ef9ab
Randall Rushing <randall.rushing@gmail.com>
2015-09-29 23:29:37 -0700

    m7: actually copy our shiny new boot animation
    Change-Id: I1ee0ce370042861b9398f3c90149328eeca84306

--- eos.mk
+++ eos.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
+# Copy Bootanimation
+PRODUCT_COPY_FILES += \
+vendor/eos/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=LRX22C.H3 BUILD_FINGERPRINT="htc/m7_google/m7:5.0.1/LRX22C.H3/434277:user/release-keys" PRIVATE_BUILD_DESC="5.11.1700.3 CL434277 release-keys"
 
 PRODUCT_NAME := eos_m7
